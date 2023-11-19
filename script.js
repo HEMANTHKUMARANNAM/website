486ba09622a7b57f1a8039d27edd83cdf46d66a7
HEMANTH KUMAR <hemanthkumar312004@gmail.com>
2023-11-19 11:58:39 +0530

    add

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const fs = require('fs'); // For Node.js environment
+
 function calculateRoots() {
     // Get coefficients from user input
     var a = parseFloat(document.getElementById('a').value);
@@ -11,8 +13,20 @@ function calculateRoots() {
     if (discriminant >= 0) {
         var root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
         var root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
-        document.getElementById('result').innerHTML = "Root 1: " + root1 + "<br>Root 2: " + root2;
+
+        var resultText = "Root 1: " + root1 + "\nRoot 2: " + root2;
+
+        document.getElementById('result').innerHTML = resultText;
+
+        // Append values to a text file
+        appendToFile(`a: ${a}, b: ${b}, c: ${c}, Roots: ${root1}, ${root2}`);
     } else {
         document.getElementById('result').innerHTML = "The equation has no real roots.";
     }
+}
+
+function appendToFile(data) {
+    // For Node.js environment
+    fs.appendFileSync('output.txt', data + '\n', 'utf8');
+    console.log('Data appended to the file.');
 }
